d7be2f86baf7a41a956d43b4e06772db5d865759
iClusterDev <cluster.coredev@gmail.com>
2020-03-11 18:41:51 +0000

    added json-server to project

--- schema.js
+++ schema.js
@@ -22,19 +22,13 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLString }
       },
       resolve(parentValue, args) {
-        return axios.get("http://localhost:3000/customers/" + args.id).then(res => res.data);
-        // for (let i = 0; i < customers.length; i++) {
-        //   if (customers[i].id == args.id) {
-        //     return customers[i];
-        //   }
-        // }
+        return axios.get(`http://localhost:3000/customers/${args.id}`).then(res => res.data);
       }
     },
     customers: {
       type: new GraphQLList(CustomerType),
       resolve(parentValue, args) {
-        return axios.get("http://localhost:3000/customers/").then(res => res.data);
-        // return customers;
+        return axios.get(`http://localhost:3000/customers/`).then(res => res.data);
       }
     }
   }
